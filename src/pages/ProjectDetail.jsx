2b4d37d3ca4a3d6221c8c0e2b14e76ceada72133
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-08-10 18:28:20 +0530

    feat: replace fake fetch with real API call in ProjectDetail.jsx

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { projectsData } from '../data/content';
 
 export default function ProjectDetail() {
   const { id } = useParams();
-  const project = projectsData.find((p) => p.id === Number(id));
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [project, setProject] = useState();
 
-  if (!project) {
-    return (
-      <section className="bg-white dark:bg-gray-900 text-black dark:text-white p-4">
-        <p>Project not found.</p>
+  useEffect(() => {
+    async function fetchProject() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:5000/api/projects/${id}`);
+          if (!response.ok) {
+              throw new Error(`Server error: ${response.status}`);
+            }
+        const data = await response.json();
+        setProject(data);
+        setError(null);
+      } catch (err) {
+        setError('Failed to load projects. Please try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchProject();
+  }, [id])
+
+ let content;
+  if (isLoading) {
+    content = <p>Loading Project...</p>;
+  } else if (error) {
+    content = (
+      <>
+        <p className="text-red-500">{error}</p>
         <Link to="/projects" className="text-blue-600 hover:underline">
           ← Back to Projects
         </Link>
-      </section>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <h2 className="text-2xl font-bold mt-2">{project.title}</h2>
+        <p className="mt-2">{project.description}</p>
+      </>
     );
   }
 
@@ -22,8 +53,7 @@ export default function ProjectDetail() {
       <Link to="/projects" className="text-blue-600 hover:underline">
         ← Back to Projects
       </Link>
-      <h2 className="text-2xl font-bold mt-2">{project.title}</h2>
-      <p className="mt-2">{project.description}</p>
+      {content}
     </section>
   );
 }
