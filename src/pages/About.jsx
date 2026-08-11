8927cd055e5aa85aa080a98348ed9bf801de7f53
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-08-11 19:10:22 +0530

    refactor: manage API fetching with custom hook  useFetch

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import useFetch from '../hooks/useFetch'
 
 export default function About() {
-  const [aboutData, setAboutData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-   async function fetchAbout() {
-      try {
-        setIsLoading(true);
-        const response = await fetch('http://localhost:5000/api/about');
-          if (!response.ok) {
-            throw new Error(`Server error: ${response.status}`);
-          }
-        const data = await response.json();
-        setAboutData(data);
-        setError(null);
-      } catch (err) {
-        setError('Failed to load Bio. Please try again later.');
-      } finally {
-        setIsLoading(false);
-      }
-    }
-  fetchAbout();
-  }, [])
+  const { data: about, isLoading, error } = useFetch("http://localhost:5000/api/about");
 
   let content;
 
@@ -32,7 +11,7 @@ export default function About() {
   } else if (error) {
     content = <p className='text-red-500'>{error}</p>;
   } else {
-    content = <p>{aboutData.bio}</p>;
+    content = <p>{about.bio}</p>;
   }
   return (
     <section className="bg-white dark:bg-gray-900 text-black dark:text-white p-4">
