2e1c71b1fba94f5027d0e469f26442d24d03e1de
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-08-14 17:31:41 +0530

    added Login.jsx import,export

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import ProjectDetail from "./ProjectDetail";
+import Login from "./Login";
 
-export { Home, About, Projects, Contact, ProjectDetail };
+export { Home, About, Projects, Contact, ProjectDetail, Login };
